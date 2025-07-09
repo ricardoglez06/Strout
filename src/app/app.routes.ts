3ae0a5e4832dae363727aef44bb3f3dfaf860ee9
Ricardoo <angelricardoglezsoto@gmail.com>
2025-07-09 10:31:15 -0600

    feat: add routing, sign up page design, and organize folders and files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PublicLayout } from '@layouts/public-layout/public-layout'
+import { Landing } from '@pages/landing/landing'
+import { Login } from '@pages/login/login'
+import { Signup } from '@pages/signup/signup'
+
+import { MainLayout } from '@layouts/main-layout/main-layout'
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: PublicLayout,
+        children: [
+            { path: '', component: Landing },
+            { path: 'login', component: Login },
+            { path: 'signup', component: Signup },
+        ]
+    },
+    {
+        path: '',
+        component: MainLayout,
+        children: []
+    },
+    { path: '**', redirectTo: '' }
+];
---
+++ src/app/layouts/public-layout/public-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router'
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [ RouterOutlet ],
+  templateUrl: './public-layout.html',
+  styles: ``
+})
+export class PublicLayout {
+
+}
